ba3cc0f17196833a7581c2bf77cf7ad3e53c4687
unknown <mgrubisi1@pmfst.hr>
2023-12-09 20:28:23 +0100

    Popravljen kod za log in (nadam se da sad radi)

--- log_in.php
+++ log_in.php
@@ -1,3 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sign_up.css">
+    <title> Prijava </title>
+</head>
+<body>
+    <div class="container">
+
+        <h1>Prijava</h1>
+
+        <form name="prijava" method="post" action="login.php">
+
+            <div class="input">
+
+                <label for="username">Korisničko ime</b></label>
+                <input type="text" name="username" required>
+
+                <label for="password">Lozinka</label>
+                <input type="password" name="password" required>
+
+            </div>
+
+            <input type="submit" name="submit_login" value="Prijavi se">
+      
+        </form>
+
+    </div>
+
+</body>
+</html>
 <?php
 
     include("pomocne_funkcije.php");
@@ -40,36 +73,3 @@
 
       
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="sign_up.css">
-    <title> Prijava </title>
-</head>
-<body>
-    <div class="container">
-
-        <h1>Prijava</h1>
-
-        <form name="prijava" method="post" action="login.php">
-
-            <div class="input">
-
-                <label for="username">Korisničko ime</b></label>
-                <input type="text" name="username" required>
-
-                <label for="password">Lozinka</label>
-                <input type="password" name="password" required>
-
-            </div>
-
-            <input type="submit" name="submit_login" value="Prijavi se">
-      
-        </form>
-
-    </div>
-
-</body>
-</html>
